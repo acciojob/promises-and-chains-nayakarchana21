df259efc9c562939ada6cc21c0d159b95b4c561f
Archana Nayak <archananayak212002@gmail.com>
2025-06-17 21:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+document.getElementById('form').addEventListener('submit',(e) => {
+  e.preventDefault();
+
+  const age = document.getElementById('age').value.trim();
+  const name = document.getElementById('name').value.trim();
+
+  new Promise((resolve, reject) => {
+    if (age >= 18) {
+      resolve(`Hello ${name}, you are eligible to vote.`);
+    } else {
+      reject(`Sorry ${name}, you are not eligible to vote.`);
+    }
+ })
+    .then((msg) => {
+      alert(msg);
+    })
+    .catch((msg) => {
+      alert(msg);
+    });
+});
+
